e9b8db3bfb5431fac8fffef89b899d2de985999c
thin <thin@herenow.io>
2016-04-11 16:01:54 +0800

    add google autocomplete for from and to address

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,6 +1,13 @@
 var roundtripMap = {
 
     init: function() {
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        }else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+
         handler = Gmaps.build('Google');
         handler.buildMap({
                 provider: {
@@ -13,15 +20,7 @@ var roundtripMap = {
                 }
             },
             function(){
-                markers = handler.addMarkers([
-                    {
-                        "lat": 1.317907,
-                        "lng": 103.843643,
-                        "infowindow": "hello!"
-                    }
-                ]);
-                handler.bounds.extendWith(markers);
-                handler.fitMapToBounds();
+
             }
         );
 
@@ -35,8 +34,6 @@ var roundtripMap = {
             }
         });
 
-        var api_key = "#{@google_apikey}"
-        console.log(api_key)
 
         $("#swapIcon").on("click",function(){
 
@@ -46,9 +43,54 @@ var roundtripMap = {
             $inputC.val($inputD.val());
             $inputD.val(temp);
 
-        })
+        }) ;
+
+        function showPosition(position) {
+            lat = position.coords.latitude;
+            lng = position.coords.longitude;
+
+            handler.getMap().setCenter(new google.maps.LatLng(lat,lng));
+
+            handler.getMap().setZoom(13)
+
+
+        }
+
+        var  from_autocomplete, to_autocomplete;
+
+
+        from_autocomplete = new google.maps.places.Autocomplete(
+            /** @type {HTMLInputElement} */(document.getElementById('from_input')),
+            { types: ['geocode'] });
+        // When the user selects an address from the dropdown,
+        // populate the address fields in the form.
+        google.maps.event.addListener(from_autocomplete, 'place_changed', function() {
+            fillInAddress();
+        });
+
+        to_autocomplete = new google.maps.places.Autocomplete(
+            /** @type {HTMLInputElement} */(document.getElementById('to_input')),
+            { types: ['geocode'] });
+        // When the user selects an address from the dropdown,
+        // populate the address fields in the form.
+        google.maps.event.addListener(to_autocomplete, 'place_changed', function() {
+            fillInAddress();
+        });
+
+
+        // [START region_fillform]
+        function fillInAddress() {
+            // Get the place details from the autocomplete object.
+            var from_place = from_autocomplete.getPlace();
+            var to_place = to_autocomplete.getPlace();
+            console.log("from_place",from_place)
+            console.log("to_place",to_place)
+
+        }
 
     }
+
+
 }
 
 
