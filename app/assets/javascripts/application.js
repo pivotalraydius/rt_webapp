ebb26ca3940852cd83b5832998626e20383a767c
thinei <thinei.khaing@code2lab.com>
2016-04-14 21:42:55 +0800

    add gmap copy js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require underscore-min
-//= require gmaps/google
 //= require_tree .
